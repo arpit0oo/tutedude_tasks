c04dbfbac256273391d7be1dd216a340c8bb1dc7
arpit0oo <arpitlko07@gmail.com>
2026-01-26 17:31:24 +0530

    Tutedude 3rd Assignment done!!

--- Assignment 2/Task1.py
+++ Assignment 2/Task1.py
@@ -1,2 +1,2 @@
 num = int(input("Enter the number : "))
-print(f"{num} is an Even Number.") if num%2==0 else print(f"{num} is an Odd Number.")
+print(f"{num} is an Even Number.") if num%2==0 else print(f"{num} is an Odd Number.") # This is called Tesnary operator 
--- Assignment 2/Task2.py
+++ Assignment 2/Task2.py
@@ -1,5 +1,5 @@
 n = int(input("Enter The Number : "))
 sum = 0
-for  i in range(1,n+1):
-     sum += i
+for  i in range(n+1):
+     sum += i # After each loop value of sum will be increamented by i(range)
 print(f"The sum of numbers in from 1 to {n} is: {sum}")
